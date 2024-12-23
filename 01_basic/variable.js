b6df4ed276d04732863a6e615075832fd8fa7c99
Pal Rahul <147297671+rahulcse129@users.noreply.github.com>
2024-12-23 11:51:27 +0000

    variable

---
+++ 01_basic/variable.js
@@ -0,0 +1,7 @@
+const accountid = 144553   
+let accountEmail = "ashidius@gmail.com"
+var accountPassword = "1234"
+accountCity = "Surat"
+let accountFire;
+
+ console.table([accountid, accountEmail, accountPassword, accountCity, accountFire])
